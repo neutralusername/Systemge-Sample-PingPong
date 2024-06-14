42a1a7ad66d7c58cc09ebba446b5aa11807294e0
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-14 09:59:46 +0200

    refactoring

--- frontend/root.js
+++ frontend/root.js
@@ -16,8 +16,8 @@ export class root extends React.Component {
             (this.state.WS_CONNECTION.onmessage = (event) => {
                 let message = JSON.parse(event.data);
                 switch (message.topic) {
-                    case "pingPongTestSuccessfull":
-                        console.log("Ping pong test successfull")
+                    case "pong":
+                        console.log("PONG")
                         break;
                     case "error":
                         let errorMessage = message.payload.split("->").reverse()[0]
