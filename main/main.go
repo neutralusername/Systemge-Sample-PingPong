0b161c00faad5d2da3762d460fd8ae90f209e5fb
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-23 07:30:01 +0200

    changed resolver to tls connection

--- main/main.go
+++ main/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 const RESOLVER_ADDRESS = "127.0.0.1:60000"
+const RESOLVER_NAME_INDICATION = "127.0.0.1"
+const RESOLVER_TLS_CERT_PATH = "MyCertificate.crt"
 const WEBSOCKET_PORT = ":8443"
 const HTTP_PORT = ":8080"
 
@@ -18,21 +20,21 @@ func main() {
 	Module.NewBrokerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH).Start()
 
 	clientApp := Module.NewClient(&Module.ClientConfig{
-		Name:            "clientApp",
-		ResolverAddress: RESOLVER_ADDRESS,
-		LoggerPath:      ERROR_LOG_FILE_PATH,
+		Name:                   "clientApp",
+		ResolverAddress:        RESOLVER_ADDRESS,
+		ResolverNameIndication: RESOLVER_NAME_INDICATION,
+		ResolverTLSCertPath:    RESOLVER_TLS_CERT_PATH,
+		LoggerPath:             ERROR_LOG_FILE_PATH,
 	}, app.New, nil)
 	clientWebsocketHTTP := Module.NewCompositeClientWebsocketHTTP(&Module.ClientConfig{
-		Name:             "clientWebsocketHTTP",
-		ResolverAddress:  RESOLVER_ADDRESS,
-		LoggerPath:       ERROR_LOG_FILE_PATH,
-		WebsocketPattern: "/ws",
-		WebsocketPort:    WEBSOCKET_PORT,
-		WebsocketCert:    "",
-		WebsocketKey:     "",
-		HTTPPort:         HTTP_PORT,
-		HTTPCert:         "",
-		HTTPKey:          "",
+		Name:                   "clientWebsocketHTTP",
+		ResolverAddress:        RESOLVER_ADDRESS,
+		ResolverNameIndication: RESOLVER_NAME_INDICATION,
+		ResolverTLSCertPath:    RESOLVER_TLS_CERT_PATH,
+		LoggerPath:             ERROR_LOG_FILE_PATH,
+		WebsocketPattern:       "/ws",
+		WebsocketPort:          WEBSOCKET_PORT,
+		HTTPPort:               HTTP_PORT,
 	}, appWebsocketHTTP.New, nil)
 	Module.StartCommandLineInterface(Module.NewMultiModule(
 		clientApp,
