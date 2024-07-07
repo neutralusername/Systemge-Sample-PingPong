6dc3b68ea1fdf554d8f89cac3da1757a3553c284
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-07 19:17:03 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -35,7 +35,6 @@ func main() {
 			LoggerPath:                ERROR_LOG_FILE_PATH,
 			ResolverEndpoint:          TcpEndpoint.New(config.SERVER_IP+":"+Utilities.IntToString(config.RESOLVER_PORT), config.SERVER_NAME_INDICATION, Utilities.GetFileContent(config.CERT_PATH)),
 			SyncResponseTimeoutMs:     1000,
-			BrokerHeartbeatIntervalMs: 100,
 			TopicResolutionLifetimeMs: 10000,
 		}, app.New()),
 		Node.New(Config.Node{
@@ -43,7 +42,6 @@ func main() {
 			LoggerPath:                ERROR_LOG_FILE_PATH,
 			ResolverEndpoint:          TcpEndpoint.New(config.SERVER_IP+":"+Utilities.IntToString(config.RESOLVER_PORT), config.SERVER_NAME_INDICATION, Utilities.GetFileContent(config.CERT_PATH)),
 			SyncResponseTimeoutMs:     1000,
-			BrokerHeartbeatIntervalMs: 100,
 			TopicResolutionLifetimeMs: 10000,
 		}, applicationWebsocketHTTP),
 	))
