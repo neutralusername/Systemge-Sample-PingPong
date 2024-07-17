ca9d9d4875238dd40f6e80700c99b233610944bc
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-17 16:40:26 +0200

    implemented recent systemge changes

--- main/main.go
+++ main/main.go
@@ -17,60 +17,47 @@ import (
 const ERROR_LOG_FILE_PATH = "error.log"
 
 func main() {
-	err := Node.New(Config.Node{
-		Name:   "nodeResolver",
-		Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
-	}, Resolver.New(Config.Resolver{
-		Server:       TcpServer.New(60000, "MyCertificate.crt", "MyKey.key"),
-		ConfigServer: TcpServer.New(60001, "MyCertificate.crt", "MyKey.key"),
+	Module.StartCommandLineInterface(Module.NewMultiModule(true,
+		Node.New(Config.Node{
+			Name:   "nodeResolver",
+			Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
+		}, Resolver.New(Config.Resolver{
+			Server:       TcpServer.New(60000, "MyCertificate.crt", "MyKey.key"),
+			ConfigServer: TcpServer.New(60001, "MyCertificate.crt", "MyKey.key"),
 
-		TcpTimeoutMs: 5000,
-	})).Start()
-	if err != nil {
-		panic(err)
-	}
+			TcpTimeoutMs: 5000,
+		})),
+		Node.New(Config.Node{
+			Name:   "nodeBrokerApp",
+			Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
+		}, Broker.New(Config.Broker{
+			Server:       TcpServer.New(60002, "MyCertificate.crt", "MyKey.key"),
+			Endpoint:     TcpEndpoint.New("127.0.0.1:60002", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
+			ConfigServer: TcpServer.New(60003, "MyCertificate.crt", "MyKey.key"),
 
-	err = Node.New(Config.Node{
-		Name:   "nodeBrokerApp",
-		Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
-	}, Broker.New(Config.Broker{
-		Server:       TcpServer.New(60002, "MyCertificate.crt", "MyKey.key"),
-		Endpoint:     TcpEndpoint.New("127.0.0.1:60002", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
-		ConfigServer: TcpServer.New(60003, "MyCertificate.crt", "MyKey.key"),
+			SyncTopics:  []string{topics.PINGPONG},
+			AsyncTopics: []string{topics.PING},
 
-		SyncTopics:  []string{topics.PINGPONG},
-		AsyncTopics: []string{topics.PING},
+			ResolverConfigEndpoint: TcpEndpoint.New("127.0.0.1:60001", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
 
-		ResolverConfigEndpoint: TcpEndpoint.New("127.0.0.1:60001", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
+			SyncResponseTimeoutMs: 10000,
+			TcpTimeoutMs:          5000,
+		})),
+		Node.New(Config.Node{
+			Name:   "nodeBrokerWebsocketHTTP",
+			Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
+		}, Broker.New(Config.Broker{
+			Server:       TcpServer.New(60004, "MyCertificate.crt", "MyKey.key"),
+			Endpoint:     TcpEndpoint.New("127.0.0.1:60004", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
+			ConfigServer: TcpServer.New(60005, "MyCertificate.crt", "MyKey.key"),
 
-		SyncResponseTimeoutMs: 10000,
-		TcpTimeoutMs:          5000,
-	})).Start()
-	if err != nil {
-		panic(err)
-	}
+			AsyncTopics: []string{topics.PONG},
 
-	err = Node.New(Config.Node{
-		Name:   "nodeBrokerWebsocketHTTP",
-		Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
-	}, Broker.New(Config.Broker{
-		Server:       TcpServer.New(60004, "MyCertificate.crt", "MyKey.key"),
-		Endpoint:     TcpEndpoint.New("127.0.0.1:60004", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
-		ConfigServer: TcpServer.New(60005, "MyCertificate.crt", "MyKey.key"),
+			ResolverConfigEndpoint: TcpEndpoint.New("127.0.0.1:60001", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
 
-		AsyncTopics: []string{topics.PONG},
-
-		ResolverConfigEndpoint: TcpEndpoint.New("127.0.0.1:60001", "example.com", Utilities.GetFileContent("MyCertificate.crt")),
-
-		SyncResponseTimeoutMs: 10000,
-		TcpTimeoutMs:          5000,
-	})).Start()
-	if err != nil {
-		panic(err)
-	}
-
-	applicationWebsocketHTTP := appWebsocketHTTP.New()
-	Module.StartCommandLineInterface(Module.NewMultiModule(
+			SyncResponseTimeoutMs: 10000,
+			TcpTimeoutMs:          5000,
+		})),
 		Node.New(Config.Node{
 			Name:   "nodeApp",
 			Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
@@ -78,6 +65,6 @@ func main() {
 		Node.New(Config.Node{
 			Name:   "nodeAppWebsocketHTTP",
 			Logger: Utilities.NewLogger(ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, ERROR_LOG_FILE_PATH, nil),
-		}, applicationWebsocketHTTP),
+		}, appWebsocketHTTP.New()),
 	))
 }
