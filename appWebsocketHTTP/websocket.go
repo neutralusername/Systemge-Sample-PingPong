b0398af565c55d22e50d87da0f15d9565acbdd3c
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-22 11:30:11 +0200

    refactoring

--- appWebsocketHTTP/websocket.go
+++ appWebsocketHTTP/websocket.go
@@ -10,6 +10,29 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+func (app *AppWebsocketHTTP) GetWebsocketComponentConfig() *Config.Websocket {
+	return &Config.Websocket{
+		Pattern: "/ws",
+		Http: &Config.Http{
+			Server: &Config.TcpServer{
+				Port: 8443,
+			},
+			Blacklist: []string{},
+			Whitelist: []string{},
+		},
+		HandleClientMessagesSequentially: false,
+		ClientMessageCooldownMs:          0,
+		ClientWatchdogTimeoutMs:          20000,
+		Upgrader: &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	}
+}
+
 func (app *AppWebsocketHTTP) GetWebsocketMessageHandlers() map[string]Node.WebsocketMessageHandler {
 	return map[string]Node.WebsocketMessageHandler{}
 }
@@ -36,23 +59,4 @@ func (app *AppWebsocketHTTP) OnConnectHandler(node *Node.Node, websocketClient *
 
 func (app *AppWebsocketHTTP) OnDisconnectHandler(node *Node.Node, websocketClient *Node.WebsocketClient) {
 	println("websocket client disconnected")
-}
-
-func (app *AppWebsocketHTTP) GetWebsocketComponentConfig() *Config.Websocket {
-	return &Config.Websocket{
-		Pattern: "/ws",
-		Server: &Config.TcpServer{
-			Port: 8443,
-		},
-		HandleClientMessagesSequentially: false,
-		ClientMessageCooldownMs:          0,
-		ClientWatchdogTimeoutMs:          20000,
-		Upgrader: &websocket.Upgrader{
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		},
-	}
 }
