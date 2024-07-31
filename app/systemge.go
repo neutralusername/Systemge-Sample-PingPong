d8325ab30ad6acb6cb5758d3e93d3efb9dd7d56e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-31 08:27:31 +0200

    removed internal logs

--- app/systemge.go
+++ app/systemge.go
@@ -24,7 +24,7 @@ func (app *App) GetAsyncMessageHandlers() map[string]Node.AsyncMessageHandler {
 	return map[string]Node.AsyncMessageHandler{
 		topics.PING: func(node *Node.Node, message *Message.Message) error {
 			app.pingsReceived++
-			/* if app.pingsReceived == 2 {
+			/* 	if app.pingsReceived == 2 {
 				startedAt = time.Now()
 			}
 			if app.pingsReceived == 100001 {
--- main/main.go
+++ main/main.go
@@ -37,13 +37,11 @@ func main() {
 	},
 		Node.New(&Config.NewNode{
 			NodeConfig: &Config.Node{
-				Name:                      "nodeApp",
-				RandomizerSeed:            Tools.GetSystemTime(),
-				InfoLoggerPath:            LOGGER_PATH,
-				WarningLoggerPath:         LOGGER_PATH,
-				ErrorLoggerPath:           LOGGER_PATH,
-				InternalInfoLoggerPath:    LOGGER_PATH,
-				InternalWarningLoggerPath: LOGGER_PATH,
+				Name:              "nodeApp",
+				RandomizerSeed:    Tools.GetSystemTime(),
+				InfoLoggerPath:    LOGGER_PATH,
+				WarningLoggerPath: LOGGER_PATH,
+				ErrorLoggerPath:   LOGGER_PATH,
 			},
 			SystemgeConfig: &Config.Systemge{
 				HandleMessagesSequentially: false,
@@ -74,13 +72,11 @@ func main() {
 		}, app.New()),
 		Node.New(&Config.NewNode{
 			NodeConfig: &Config.Node{
-				Name:                      "nodeWebsocketHTTP",
-				RandomizerSeed:            Tools.GetSystemTime(),
-				InfoLoggerPath:            LOGGER_PATH,
-				WarningLoggerPath:         LOGGER_PATH,
-				ErrorLoggerPath:           LOGGER_PATH,
-				InternalInfoLoggerPath:    LOGGER_PATH,
-				InternalWarningLoggerPath: LOGGER_PATH,
+				Name:              "nodeWebsocketHTTP",
+				RandomizerSeed:    Tools.GetSystemTime(),
+				InfoLoggerPath:    LOGGER_PATH,
+				WarningLoggerPath: LOGGER_PATH,
+				ErrorLoggerPath:   LOGGER_PATH,
 			},
 			SystemgeConfig: &Config.Systemge{
 				HandleMessagesSequentially: false,
